cdca82f7a09e35c5a072fbc428d2a56f2b934a56
knextsunj <knextsunj@users.noreply.github.com>
2024-05-25 03:28:57 +0530

    New practice- ref/out

--- MyFirstProject/classes/IndexRangeTest.cs
+++ MyFirstProject/classes/IndexRangeTest.cs
@@ -7,21 +7,21 @@ namespace MyFirstProject.classes
 {
     public class IndexRangeTest
     {
-        public static void Main() {
-            Console.WriteLine("Enter value::: ");
-            string? s1 = Console.ReadLine();
-            Console.WriteLine($"Value entered is::::{s1}");
-            string[] arr = new string[] { "he", "be","me","ze","qe","fe","ve","ne",};
-            //Range
-            string[] leftArr = arr[..2]; //0,1
-            string[] rightArr = arr[3..]; // After first 3 elements
-            foreach(string str in leftArr) {
-                Console.WriteLine(str);
-            }
-            Console.WriteLine("----------------------------------------");
-            foreach(string str in rightArr) {
-                Console.WriteLine(str);
-            }
-        }
+        // public static void Main() {
+        //     Console.WriteLine("Enter value::: ");
+        //     string? s1 = Console.ReadLine();
+        //     Console.WriteLine($"Value entered is::::{s1}");
+        //     string[] arr = new string[] { "he", "be","me","ze","qe","fe","ve","ne",};
+        //     //Range
+        //     string[] leftArr = arr[..2]; //0,1
+        //     string[] rightArr = arr[3..]; // After first 3 elements
+        //     foreach(string str in leftArr) {
+        //         Console.WriteLine(str);
+        //     }
+        //     Console.WriteLine("----------------------------------------");
+        //     foreach(string str in rightArr) {
+        //         Console.WriteLine(str);
+        //     }
+        // }
     }
 }
---
+++ MyFirstProject/classes/RefTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.classes
+{
+    public class RefTest
+    {
+
+        private int y =10;
+        private int result1;
+
+        private int z = 25;
+
+        public static void Main(string[] args) {
+            RefTest refTest= new RefTest();
+            Console.WriteLine($"y={refTest.y}");
+            refTest.manipulate(ref refTest.y,out refTest.result1,in refTest.z);
+            Console.WriteLine($"now y={refTest.y}");
+            Console.WriteLine("result1="+refTest.result1);
+            refTest.switches(2);
+        }
+ 
+        public void manipulate(ref int x,out int result,in int z) {
+            Console.WriteLine("x=" + x);
+            Console.WriteLine($"z={z}");
+            x += 10;
+            result = x;
+            Console.WriteLine("Now x=" + x);
+        }
+
+        public void switches(int ch) {
+            switch(ch) {
+                case 0:
+                Console.WriteLine("0");
+                break;
+                case 1:
+                case 2:
+                Console.WriteLine("1 or 2");
+                break;
+                default:
+                Console.WriteLine("default");
+                    break;  
+            }
+        }
+    }
+
+
+}
